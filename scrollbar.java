ec548d70c32a8983281fd532707b2742d26add63
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-08 20:26:58 +0530

    Create scrollbar.java

---
+++ scrollbar.java
@@ -0,0 +1,18 @@
+import javax.swing.*;  
+class scrollbar  
+{  
+scrollbar()
+{  
+ JFrame f= new JFrame("Scrollbar Example");  
+ JScrollBar s=new JScrollBar();  
+s.setBounds(100,100, 50,100);  
+f.add(s);  
+f.setSize(400,400);  
+f.setLayout(null);  
+f.setVisible(true);  
+}  
+public static void main(String args[])  
+{  
+new scrollbar();  
+}
+}  
